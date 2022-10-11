23c31fa7d82fe46ab5d33200c81dea6072e6c2f2
zamora441 <101614191+zamora441@users.noreply.github.com>
2022-10-11 00:06:04 -0600

    Addition of the routes model description

--- front_soap/models/routes.js
+++ front_soap/models/routes.js
@@ -1,3 +1,8 @@
+/*
+Creacion de objetos con las rutas publicas y privadas utilizadas para el redireccionamiento
+del login de acuerdo al rol del usuario
+Oscar Zamora Ramirez
+*/
 export const PublicRoutes = {
     LOGIN: "login",
 };

fb99740a74047814ed994c5dbcaf155c3c38499e
JeffVar00 <jeff.vargas.barrantes@est.una.ac.cr>
2022-10-11 21:43:56 -0600

    fixed responsive main

--- front_soap/components/userAdmin/AdminUI.jsx
+++ front_soap/components/userAdmin/AdminUI.jsx
@@ -1,9 +1,9 @@
 const Main = () => {
   return (
     <div className="w-full max-w-screen xl:max-w-5xl lg:ml-60 xl:ml-72">
-      <div className="flex flex-col ml-5 ">
-        <h2 className="mb-5 font-bold text-6xl md:text-7xl lg:mb-0">Menu</h2>
-        <h2 className="mb-5 font-bold text-6xl md:text-7xl lg:mb-0 text-red-600">
+      <div className="flex flex-col md:ml-5 ">
+        <h2 className="mb-5 font-bold text-5xl md:text-7xl lg:mb-0">Menu</h2>
+        <h2 className="mb-5 font-bold text-5xl md:text-7xl lg:mb-0 text-red-600">
           Administrador
         </h2>
       </div>

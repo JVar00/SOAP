e0edd372c7ba4be40816e04a7a2a7c8b8df36b58
zamora441 <101614191+zamora441@users.noreply.github.com>
2022-10-14 13:11:31 -0600

    Revert "Update layout AyA"
    This reverts commit 5a7c0c9d4931bd94bcdbb34fb75c3cb633d943b6.

--- front_soap/layouts/AyA.jsx
+++ front_soap/layouts/AyA.jsx
@@ -1,10 +1,5 @@
-import { useContext } from "react";
-import { AuthContext } from "../contexts/authContext";
 const AyA = () => {
-    const {logOut} = useContext(AuthContext)
-    return <div>Acomodo y Alisto(Private)
-        <button onClick={logOut}>Salir</button>
-    </div>;
+    return <div>Acomodo y Alisto(Private)</div>;
 };
 
 export default AyA;

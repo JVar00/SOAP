1ed73c425ba1931cc7cf463ac30fe38a15d9b2cb
zamora441 <101614191+zamora441@users.noreply.github.com>
2022-10-14 13:02:35 -0600

    Create loginService

---
+++ front_soap/services/LoginService.js
@@ -0,0 +1,11 @@
+import http from "../http-common";
+
+const getAuthentication = (userName, password) => {
+    return http.get(`filtro_User?txtUser=${userName}&txtPassword=${password}`);
+};
+
+const LogingService = {
+    getAuthentication,
+};
+
+export default LogingService;

13d084275e32357de01c3f06c2ef088063c468f9
zamora441 <101614191+zamora441@users.noreply.github.com>
2022-11-12 02:32:15 -0600

    created and finished the warehouse management service

---
+++ front_soap/services/ShelfService.js
@@ -0,0 +1,31 @@
+import http from "../http-common";
+
+const getAll = () => {
+    return http.get("/shelfs");
+};
+
+const get = (id) => {
+    return http.get(`/shelfs/${id}`);
+};
+
+const create = (data) => {
+    return http.post("/shelfs", data);
+};
+
+const update = (data, id) => {
+    return http.put(`/shelfs/${id}`, data);
+};
+
+const remove = (id) => {
+    return http.delete(`/shelfs/${id}`);
+};
+
+const ShelfServiceData = {
+    getAll,
+    get,
+    remove,
+    create,
+    update,
+};
+
+export default ShelfServiceData;

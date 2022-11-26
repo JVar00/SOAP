28a187550042ab8c23fd1f4ba361f3e5ffcf8369
JeffVar00 <jeff.vargas.barrantes@est.una.ac.cr>
2022-11-25 18:19:28 -0600

    updated http for future uses

--- front_soap/src/http-common.js
+++ front_soap/src/http-common.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "http://localhost:8000/api/", //url del api
+  //baseURL: "https://df62-186-15-227-74.ngrok.io/api", //url del api
   headers: {
     "Content-type": "application/json",
+    //"ngrok-skip-browser-warning": "true",
   },
 });
